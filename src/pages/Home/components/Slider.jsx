cfa3a0851122cb4936eb93cfe54eb47cbb64c745
mobeen68 <mobeene98@gmail.com>
2022-10-16 17:14:00 +0500

    added icons

--- src/pages/Home/components/Slider.jsx
+++ src/pages/Home/components/Slider.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Carousel } from "flowbite-react";
 import { config } from "../../../config";
+import { BsArrowLeftCircle, BsArrowRightCircle } from "react-icons/bs";
 
 const Slider = () => {
   return (
     <div className="h-56 sm:h-64 xl:h-80 2xl:h-96 ">
-      <Carousel>
+      <Carousel
+        leftControl={<BsArrowLeftCircle size={"2rem"} color="grey" />}
+        rightControl={<BsArrowRightCircle size={"2rem"} color="grey" />}
+      >
         {config.images.map(({ link }) => (
           <img src={link} alt="..." className="object-fill h-auto" />
         ))}
--- src/pages/Home/components/Team.jsx
+++ src/pages/Home/components/Team.jsx
@@ -2,13 +2,19 @@ import { Carousel } from "flowbite-react";
 import React from "react";
 import { config } from "../../../config";
 import TeamCard from "./TeamCard";
+import { BsArrowLeftCircle, BsArrowRightCircle } from "react-icons/bs";
 
 const Team = () => {
   return (
     <section class="bg-white dark:bg-gray-900">
-      <div className="uppercase text-center text-4xl font-bold pt-2">Our team</div>
+      <div className="uppercase text-center text-4xl font-bold pt-2">
+        Our team
+      </div>
       <div style={{ height: "70vh" }} className="h-56 sm:h-64 xl:h-80 2xl:h-96">
-        <Carousel>
+        <Carousel
+          leftControl={<BsArrowLeftCircle size={"2rem"} color="grey" />}
+          rightControl={<BsArrowRightCircle size={"2rem"} color="grey" />}
+        >
           {config.Team.map(({ name, category, description }) => (
             <div className="flex justify-center ">
               <TeamCard name={name} cat={category} des={description} />
